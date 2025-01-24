2e5d41dfd84f42fb1c63930dd9a35c8ac5e93cd0
jschreye <jeremieschreyer@gmail.com>
2025-01-24 13:09:43 +0100

    modif entity

--- app/Core/Data/Entity/User.cs
+++ app/Core/Data/Entity/User.cs
@@ -4,14 +4,36 @@ namespace Core.Data.Entity
     public class User
     {
         public int Id { get; set; }
-        public string ? Lastname { get; set; }
-        public string ? Firstname { get; set; }
-        public string ? Username { get; set; }
-        public string ? Email { get; set; }
-        public string ? PasswordHash { get; set; }
-        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        
+        public string Email { get; set; } = string.Empty;
+        
+        public string Username { get; set; } = string.Empty;
+        
+        public string Lastname { get; set; } = string.Empty;
+        
+        public string Firstname { get; set; } = string.Empty;
+        
+        public string PasswordHash { get; set; } = string.Empty;
+        
+        public string? Gender { get; set; }
+        
+        public string? SexualPreferences { get; set; }
+        
+        public string? Biography { get; set; }
+        
+        // Représentation de la localisation GPS
+        public double? Latitude { get; set; }
+        
+        public double? Longitude { get; set; }
+        
+        public int PopularityScore { get; set; } = 0;
+        
         public bool IsActive { get; set; } = false;
-        public string ? ActivationToken { get; set; }
-        public string ? PasswordResetToken { get; set; }
+        
+        public string? ActivationToken { get; set; }
+        
+        public string? PasswordResetToken { get; set; }
+        
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     }
 }
