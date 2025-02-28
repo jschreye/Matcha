1c77d29fefd7e94ee44708290bcb3a3bdcd02c0d
jschreye <jeremieschreyer@gmail.com>
2025-02-28 11:48:33 +0100

    f_chat_listUser

--- app/Core/Interfaces/Services/IChatService.cs
+++ app/Core/Interfaces/Services/IChatService.cs
@@ -1,14 +1,11 @@
+using Core.Data.Entity;
+
 namespace Core.Interfaces.Services
 {
     public interface IChatService
     {
-        // Permet d'envoyer un message
-        void SendMessage(string user, string message);
-
-        // Événement déclenché lorsqu'un nouveau message est reçu
-        event Action<string> OnMessageReceived;
-
-        // Propriété permettant d'accéder à l'historique des messages
-        IReadOnlyList<string> Messages { get; }
+        IReadOnlyList<Message> Messages { get; }
+        event Action<Message>? OnMessageReceived;
+        void SendMessage(int senderId, int receiverId, string contenu);
     }
 }
